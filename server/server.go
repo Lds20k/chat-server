4b38d25f49d9e26a727031c5c4d8e5364cc896e8
Lucas da Silva dos Santos <lucassilva.lds@outlook.com>
2022-11-20 19:25:05 -0300

    feat: private message and user list

--- server/server.go
+++ server/server.go
@@ -10,10 +10,16 @@ import (
 
 type client chan<- string // canal de mensagem
 
+type user struct {
+	nick   string
+	chanel chan string
+}
+
 var (
 	entering = make(chan client)
 	leaving  = make(chan client)
 	messages = make(chan string)
+	users    = make(map[string]user)
 )
 
 func broadcaster() {
@@ -21,6 +27,7 @@ func broadcaster() {
 	for {
 		select {
 		case msg := <-messages:
+
 			// broadcast de mensagens. Envio para todos
 			for cli := range clients {
 				cli <- msg
@@ -44,23 +51,60 @@ func handleConn(conn net.Conn) {
 	ch := make(chan string)
 	go clientWriter(conn, ch)
 
-	nick := conn.RemoteAddr().String()
-	ch <- "vc é " + nick
-	messages <- nick + " chegou!"
+	user_obj := user{
+		nick:   conn.RemoteAddr().String(),
+		chanel: ch,
+	}
+	users[user_obj.nick] = user_obj
+	fmt.Println(users)
+
+	ch <- "vc é " + user_obj.nick
+	messages <- user_obj.nick + " chegou!"
 	entering <- ch
 
 	input := bufio.NewScanner(conn)
+main_loop:
 	for input.Scan() {
 		var text string = input.Text()
-		if text[0:1] == "/" {
+		if len(text) > 0 && text[0:1] == "/" {
 			var command = strings.Split(text[1:], " ")[0]
 			var arguments = strings.Split(text, " ")[1:]
 			switch command {
 			case "nick":
 				if len(arguments) == 1 {
-					var old_nick = nick
-					nick = arguments[0]
-					fmt.Println("\"" + old_nick + "\" change nick to \"" + nick + "\"")
+					if _, ok := users[arguments[0]]; !ok {
+						var old_nick = user_obj.nick
+						user_obj.nick = arguments[0]
+						fmt.Println("\"" + old_nick + "\" change nick to \"" + user_obj.nick + "\"")
+
+						delete(users, old_nick)
+						users[user_obj.nick] = user_obj
+					} else {
+						user_obj.chanel <- "[server] Name already in use!"
+					}
+				} else {
+					user_obj.chanel <- "[server] Invalid arguments!"
+				}
+			case "exit":
+				if len(arguments) == 0 {
+					fmt.Println("\"" + user_obj.nick + "\" exit from server")
+					break main_loop
+				} else {
+					user_obj.chanel <- "[server] Invalid arguments!"
+				}
+			case "private":
+				if len(arguments) >= 2 {
+					fmt.Print("private to " + arguments[0] + ": ")
+					if user_s, ok := users[arguments[0]]; ok {
+						fmt.Println("-send-")
+						user_s.chanel <- "[private] " + user_obj.nick + ": " + strings.Join(arguments[1:], " ")
+						user_obj.chanel <- "[server] Message sended!"
+					} else {
+						fmt.Println("-not found-")
+						user_obj.chanel <- "[server] User not found!"
+					}
+				} else {
+					user_obj.chanel <- "[server] Invalid arguments!"
 				}
 			default:
 				var error_message string = "command \"" + command + "\" not found"
@@ -69,12 +113,12 @@ func handleConn(conn net.Conn) {
 			}
 
 		} else {
-			messages <- nick + ": " + text
+			messages <- user_obj.nick + ": " + text
 		}
 	}
 
 	leaving <- ch
-	messages <- nick + " se foi "
+	messages <- user_obj.nick + " saiu do servidor"
 	conn.Close()
 }
 
